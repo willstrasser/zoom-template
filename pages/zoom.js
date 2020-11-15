dd92978e896edb3711d0d8e3786c371741a7f79a
Will Strasser <wstrasser@hioscar.com>
2020-11-15 13:21:38 -0500

    styling and material ui

--- pages/zoom.js
+++ pages/zoom.js
@@ -1,10 +1,21 @@
 import {useState} from 'react';
 import html2canvas from 'html2canvas';
+import { Button } from '@material-ui/core';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+import CancelIcon from '@material-ui/icons/Cancel';
+
+import styles from './Zoom.module.scss'
+
+const ASPECT_RATION = 5/7;
+const LAYOUT_WIDTH = 100;
+const LAYOUT_HEIGHT = LAYOUT_WIDTH * ASPECT_RATION;
+const ZOOM_FACTOR = 3;
 
 export default function Zoom() {
   const onSave = (() =>{
-    document.querySelector("#grid").style.width='300vw';
-    document.querySelector("#grid").style.height='214.2vw';
+    document.querySelector("#grid").style.width=`${ZOOM_FACTOR*LAYOUT_WIDTH}vw`;
+    document.querySelector("#grid").style.height=`${ZOOM_FACTOR*LAYOUT_HEIGHT}vw`;
     document.body.style.fontSize='5rem';
     html2canvas(document.querySelector("#grid"), {
       allowTaint: true,
@@ -19,78 +30,52 @@ export default function Zoom() {
       a.click();
       document.body.removeChild(a);
 
-      document.querySelector("#grid").style.width='100vw';
-      document.querySelector("#grid").style.height='71.4vw';
+      document.querySelector("#grid").style.width=`${LAYOUT_WIDTH}vw`;
+      document.querySelector("#grid").style.height=`${LAYOUT_HEIGHT}vw`;
       document.body.style.fontSize='1rem';
     });
   })
   const array = Array.apply(null, Array(12)).map(function () {})
   return(
     <>
-      <div style={{
-        height: '71.4vw',
-        width: '100vw',
-        display: 'grid',
-        gridTemplateColumns: '25% 25% 25% 25%',
-        gridTemplateRows: '33.33% 33.33% 33.33%',
-        border: '.1em solid black',
-        position: 'relative',
-        background: 'rgba(0,0,0,.5)'
-      }} id="grid">
-        {array.map((item, index) => <Cell key={index}/>)}
+      <div className={styles.grid} id="grid">
+        {array.map((item, index) => <Cell key={index} index={index} />)}
 
-        <img src="/chrome.png" style={{
-          width: '100%',
-          position: 'absolute',
-          left: 0,
-          right: 0,
-          bottom: 0
-        }}/>
+        <img src="/chrome.png" className={styles.chrome}/>
       </div>
-      <div style={{position: 'absolute', background: 'green', top:0, padding:10}} onClick={onSave}>Export</div>
+      <div className={styles.exportButton}>
+        <Button variant="contained" color="primary" onClick={onSave}>Export</Button>
+      </div>
     </>
   );
 }
 
-const Cell = () => {
+const Cell = ({index}) => {
   const [files, setFiles] = useState([]);
-  // const [name, setName] = useState('');
-  const onChange = (e) => {
-    setFiles(event.target.files);
-  }
+  const id = `icon-button-file-${index}`
   return (
-    <div style={{width: '100%', height: '100%', border: '.1em solid black', position:'relative'}}>
-      {!files.length > 0 && <input style={{position: 'absolute'}} onChange={onChange} type="file" accept="image/*"></input>}
+    <div className={styles.cell}>
+      {!files.length > 0 && (
+        <div className={styles.upload}>
+          <input className={styles.input} onChange={() => {setFiles(event.target.files)}} type="file" id={id}  accept="image/*"></input>
+          <label htmlFor={id}>
+            <IconButton aria-label="upload picture" component="span">
+              <PhotoCamera fontSize="large" />
+            </IconButton>
+          </label>
+        </div>
+      )}
       {Array.from(files).map(file=>{
         return (
-          <div 
-            style={{
-              width: '100%', 
-              height: '100%', 
-              background: `transparent url("${URL.createObjectURL(file)}") no-repeat center center/cover`
-            }}
-          >
-            <div onClick={()=>setFiles([])} style={{position:'absolute', right: 0}}>x</div>
+          <div className={styles.image} style={{background: `transparent url("${URL.createObjectURL(file)}") no-repeat center center/cover`}}>
+            <div className={styles.closeButton} >
+              <IconButton onClick={()=>setFiles([])} aria-label="remove picture" component="span">
+                <CancelIcon />
+              </IconButton>
+            </div>
           </div>
         );
         })}
-        {/* <input
-          value={name}
-          placeholder="Enter name here"
-          onChange={(e) => setName(e.value)}
-          style={{
-            width: '100%',
-            padding: '.3em',
-            position:'absolute', 
-            bottom: 0, 
-            color: 'white', 
-            background:'rgba(0,0,0,.75)', 
-            border:'none',
-            backgroundImage:'none',
-            boxShadow: 'none',
-            fontSize: '1em',
-          }}
-        /> */}
     </div>
   );
 }
